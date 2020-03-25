ff0295ac999a418116824cef1d3cc01829fff567
svforg <svforgel89@gmail.com>
2020-03-25 21:16:57 +0500

    update migrations

---
+++ migrations/m200325_161440_create_news_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%news}}`.
+ */
+class m200325_161440_create_news_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%news}}', [
+            'id' => $this->primaryKey(),
+            'name' => $this->string(2048)->notNull(),
+            'image' => $this->string()->notNull(),
+            'status' => $this->boolean()->defaultValue(false),
+            'subject' => $this->string(2048),
+            'text' => $this->text(),
+            'created_at' => $this->integer()->defaultExpression('NOW()'),
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%news}}');
+    }
+}
